b167e16cb9fff43ebd8896c07793d1bd173bd75e
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-10-19 10:48:52 +0700

    19/10/2022

---
+++ MoveToXY/actionXY.py
@@ -0,0 +1,14 @@
+from tools import * 
+
+set_x = 4
+set_y = 4
+
+target_x = 2
+target_y = 2
+
+set_xy = {"x":set_x,"y":set_y}
+target_xy = {"x":target_x,"y":target_y}
+
+set = Point(set_xy)
+target = Point(target_xy)
+
