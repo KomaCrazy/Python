0d7e6446faeaf2d4ace448b72f157dd5740129c1
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-10-28 14:06:13 +0700

    1

--- sqlite3/Server.py
+++ sqlite3/Server.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from xml.dom.minidom import Document
 from tools import * 
 
@@ -8,6 +9,33 @@ def about():
 
 @app.route("/robot_add")
 def robot_add():
+ try:
   return jsonify(Sql.add(request.args))
+ except Exception as e:
+  return {"status":"Add Robot Fail"}
+
+@app.route("/robot_delete")
+def robot_delete():
+ try:
+  return jsonify(Sql.delete(request.args))
+ except Exception as e:
+  print(e)
+  return {"status":"delete Robot Fail"}
+
+@app.route("/robot_show")
+def robot_show():
+ try:
+  return jsonify(Sql.show(request.args))
+ except Exception as e:
+  print(e)
+  return {"status":"show Robot Fail"}
+
+@app.route("/robot_edit")
+def robot_edit():
+ try:
+  return jsonify(Sql.edit(request.args))
+ except Exception as e:
+  print(e)
+  return {"status":"Edit Robot Fail"}
 
 SERVER.Run()
--- sqlite3/tools.py
+++ sqlite3/tools.py
@@ -8,7 +8,7 @@ host = '0.0.0.0'
 port = 5000
            #show         regis       delete      edit   
 url = {"/","/robot_show","robot_add","robot_del","robot_edit"}
-
+path = "./db.sqlite"
 class TEXT :
   def Document() -> str:
     table = []
@@ -16,16 +16,45 @@ class TEXT :
       table.append(i)
     return table
 
+
+def sql_cli(data) :
+  with sqlite3.connect(path) as conn :
+    conn.execute(data)    
+
+
 class Sql :
   def add(data) -> str :
-    table = []
     name = data["name"]
-    ip = data["name"]
-    with sqlite3.connect("./db.sqlite") as conn :
-      cmd = f'insert into db(name,ip)values("{name}","{ip}");'
-      conn.execute(cmd)
+    ip = data["ip"]
+    cmd = f'insert into db(name,ip)values("{name}","{ip}");'
+    sql_cli(cmd)
+    return {"status":"Add success"}
 
-      
+  def delete(data) -> str :    
+    id = data["id"]
+    cmd = f'delete from db where id="{id}";'
+    sql_cli(cmd)
+    return {"status":"Delete success"}
+
+  def edit(data) -> str :    
+    id = data["id"]
+    name = data["name"]
+    ip = data["ip"]
+
+    cmd = f'update db set name="{name}" , ip="{ip}" where id={id};'
+    sql_cli(cmd)
+    return {"status":"Update success"}
+
+
+  def show(data) -> str :
+    with sqlite3.connect(path) as conn :
+      cmd = f'select * from db;'
+      table = []
+      for i in conn.execute(cmd):
+        table.append({"id":i[0],"name":i[1],"ip":i[2]})
+      return table
+
+
 class SERVER :
   def Run():
     app.run(host,port)
